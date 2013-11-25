73343849a23bc1a025371a4c31151892a034826b
Simon Baird <sbaird@redhat.com>
2013-11-25 15:19:11 +1000

    Improvements to vim/vundle aliases

--- vim/aliases.sh
+++ vim/aliases.sh
@@ -1,3 +1,4 @@
-alias vim_update='vim +BundleInstall +qall'
-alias vim_bootstrap='git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle && vim_update'
-alias vim_rebuild='rm -rf ~/.vim && vim_bootstrap'
+alias vim_get_bundles='vim +BundleInstall +qall'
+alias vim_update_bundles='vim +BundleInstall! +qall'
+alias vim_install='git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle && vim_update_bundles'
+alias vim_clean_install='rm -rf ~/.vim && vim_install'
