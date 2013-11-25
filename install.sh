9b8063f884a661fe1c2c537b4bcacefcdd9b4c31
Simon Baird <sbaird@redhat.com>
2013-11-25 15:32:00 +1000

    Improvements to install.sh (and rename from setup.sh)

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+dotfile_link() {
+  SUBDIR=$1
+  FILE=$2
+  TARGET=.dotfiles/$SUBDIR/$FILE
+  TS=`date +%Y%m%d%H%M%S`
+
+  cd $HOME
+  if [ -L $FILE ]; then
+    echo Leaving $FILE symlink alone
+  else
+    if [ -e $FILE ]; then
+      echo Preserving existing $FILE as $FILE.$TS
+      mv $FILE $FILE.$TS
+    fi
+    echo "Creating symlink $FILE -> $TARGET"
+    ln -s $TARGET
+  fi
+}
+
+dotfile_link bash   .bash_profile
+dotfile_link bash   .bashrc
+dotfile_link screen .screenrc
+dotfile_link vim    .vimrc
+dotfile_link git    .gitconfig
+dotfile_link git    .gitignore-global
