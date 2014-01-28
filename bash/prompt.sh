c4118a25851d493c7bee00ec2e360ac0b112d145
Simon Baird <simon.baird@gmail.com>
2014-01-29 02:02:37 +1000

    Remove git repo name from bash prompt

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -8,7 +8,8 @@ _NOCOLOR='\[\033[00m\]'
 _show_git_branch() {
   GIT_REPO=`git rev-parse --show-toplevel 2>/dev/null`
   GIT_BRANCH=`git rev-parse --abbrev-ref HEAD 2>/dev/null`
-  [ ! -z $GIT_REPO ] && echo "[`basename $GIT_REPO`:$GIT_BRANCH] "
+  #[ ! -z $GIT_REPO ] && echo "[`basename $GIT_REPO`:$GIT_BRANCH] "
+  [ ! -z $GIT_REPO ] && echo "[$GIT_BRANCH] "
 }
 
 PS1="$_MAGENTA\h:$_GREEN\w> $_YELLOW\$(_show_git_branch)$_NOCOLOR"
