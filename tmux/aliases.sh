789d838d9a11e123e9fda73f7f00c7ae7d0043e3
Simon Baird <sbaird@redhat.com>
2017-06-27 09:55:07 +1000

    tmux: Tweak startup aliases
    Want to have two different tmux sessions, one for rails and one for
    ansible and be able to attach to either easily.

--- tmux/aliases.sh
+++ tmux/aliases.sh
@@ -1,18 +1,37 @@
 tm() {
-  tmux new -s main -d
-  tmux new-window -t main -n vim
-  tmux new-window -t main -n console
-  tmux new-window -t main -n server
-  tmux new-window -t main -n test
-  tmux new-window -t main -n deploy
-  tmux new-window -t main -n log
-  tmux new-window -t main -n db
-  tmux new-window -t main -n devel
-  tmux new-window -t main -n 'prod!'
+  tmux new -s rails -d
+  tmux new-window -t rails -n vim
+  tmux new-window -t rails -n console
+  tmux new-window -t rails -n log
+  tmux new-window -t rails -n test
+  tmux new-window -t rails -n server
+  tmux new-window -t rails -n deploy
+  tmux new-window -t rails -n db
+  tmux new-window -t rails -n dev
+  tmux new-window -t rails -n qe
+  tmux new-window -t rails -n stage
+  tmux new-window -t rails -n 'prod!'
   tmux select-window -t :1
-  tmux -2 attach-session -t main
+  tmux -2 attach-session -t rails
+}
+
+tm2() {
+  tmux new -s ansible -d
+  tmux new-window -t ansible -n engops
+  tmux new-window -t ansible -n errata-tool
+  tmux new-window -t ansible -n vagrant
+  tmux new-window -t ansible -n roles1
+  tmux new-window -t ansible -n roles2
+  tmux new-window -t ansible -n deploy
+  tmux new-window -t ansible -n devel
+  tmux select-window -t :1
+  tmux -2 attach-session -t ansible
 }
 
 tma() {
-  tmux -2 a
+  tmux -2 a -t rails
+}
+
+tma2() {
+  tmux -2 a -t ansible
 }
