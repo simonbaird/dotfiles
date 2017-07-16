50da3a5458457ecf8c60d05e30a33720cffc6371
Simon Baird <simon.baird@gmail.com>
2017-07-17 09:30:13 +1000

    bash: Tweak bash prompt colours
    Also clean up the ansi code colour list

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,9 +1,25 @@
-_YELLOW='\[\033[01;33m\]'
-_GREEN='\[\033[01;32m\]'
-_BLUE='\[\033[01;34m\]'
-_MAGENTA='\[\033[01;35m\]'
-_CYAN='\[\033[01;36m\]'
-_NOCOLOR='\[\033[00m\]'
+
+_ansi_code() {
+  echo '\[\033['$1'm\]'
+}
+
+_Black=$(        _ansi_code '0;30')
+_Red=$(          _ansi_code '0;31')
+_Green=$(        _ansi_code '0;32')
+_Orange=$(       _ansi_code '0;33')
+_Blue=$(         _ansi_code '0;34')
+_Purple=$(       _ansi_code '0;35')
+_Cyan=$(         _ansi_code '0;36')
+_LightGray=$(    _ansi_code '0;37')
+_DarkGray=$(     _ansi_code '1;30')
+_LightRed=$(     _ansi_code '1;31')
+_LightGreen=$(   _ansi_code '1;32')
+_Yellow=$(       _ansi_code '1;33')
+_LightBlue=$(    _ansi_code '1;34')
+_LightPurple=$(  _ansi_code '1;35')
+_LightCyan=$(    _ansi_code '1;36')
+_White=$(        _ansi_code '1;37')
+_ClearColor=$(   _ansi_code '0'   )
 
 _show_git_branch() {
   GIT_REPO=`git rev-parse --show-toplevel 2>/dev/null`
@@ -12,4 +28,4 @@ _show_git_branch() {
   [ ! -z $GIT_REPO ] && echo "[$GIT_BRANCH] "
 }
 
-PS1="$_MAGENTA\h:$_GREEN\w> $_YELLOW\$(_show_git_branch)$_NOCOLOR"
+PS1="$_Cyan\h:$_Yellow\w> $_LightBlue\$(_show_git_branch)$_ClearColor"
