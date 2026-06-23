4f0b8ece216bd3b8728a1161c8042e5435d05547
Simon Baird <simon.baird@gmail.com>
2026-06-23 16:03:25 -0400

    bash: Path updates

--- bash/path.sh
+++ bash/path.sh
@@ -1,2 +1,22 @@
-PATH=$PATH:$HOME/.rvm/bin
-PATH=$PATH:$HOME/bin
+# My own misc bash stuff
+PATH="$HOME/bin:$PATH"
+
+# For `go install`ed stuff
+PATH="$HOME/go/bin:$PATH"
+
+# For rust stuff
+PATH="$HOME/.cargo/bin:$PATH"
+
+# For asdf
+PATH="$HOME/.asdf/shims:$PATH"
+
+# Ruby version manager
+PATH="$HOME/.rvm/bin:$PATH"
+
+# For claude (and etc)
+PATH="$HOME/.local/bin:$PATH"
+
+# For local Conforma builds
+PATH="$HOME/Code/ec/cli/dist:$PATH"
+
+export PATH
