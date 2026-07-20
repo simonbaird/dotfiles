07185d8737d38b475df29b58895bdc07ec8573d3
Simon Baird <simon.baird@gmail.com>
2026-07-20 12:47:35 -0400

    bash: Installers for gcloud and claude

---
+++ bash/bin/install-claude.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+curl -fsSL https://claude.ai/install.sh -o claude-install.sh
+chmod +x claude-install.sh
+./claude-install.sh
---
+++ bash/bin/install-gcloud.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# https://docs.cloud.google.com/sdk/docs/install-sdk#rpm
+
+sudo tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
+[google-cloud-cli]
+name=Google Cloud CLI
+baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el9-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=0
+gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOM
+
+sudo dnf install libxcrypt-compat.x86_64
+
+sudo dnf install google-cloud-cli
